494aae593bff22a6db8a001c0e6d8457ab0c6937
Sumin Son <65898889+codeamor@users.noreply.github.com>
2021-03-01 18:13:48 +0900

    Update src/assets/styles/global-style.ts
    Co-authored-by: Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>

--- src/assets/styles/global-style.ts
+++ src/assets/styles/global-style.ts
@@ -5,9 +5,7 @@ const GlobalStyle = createGlobalStyle`
   ${reset}
   * {
     box-sizing: border-box;
-  }
-  body{
-    font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+    font-family: 'Noto Sans KR', sans-serif;
   }
   a {
     color: inherit;
